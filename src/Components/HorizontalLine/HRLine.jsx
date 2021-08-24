c223e6a859a9f870afbf7d629411495f396056f1
Ransingh88 <rajaransingh@gmail.com>
2021-08-24 19:57:32 +0530

    code update

--- src/Components/HorizontalLine/HRLine.jsx
+++ src/Components/HorizontalLine/HRLine.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Line = styled.div`
-height: 10px;
-background-color: 1px solid red;
-
+height: ${props=> props.height === undefined ? "1px" : props.height};
+background-color: ${props=> props.color === undefined ? "#ddd" : props.color};
+width: ${props=>props.width === undefined ? "100%" : props.width};
+margin: auto;
 `
 
 
-const HRLine = () => {
+const HRLine = ({width,color,height}) => {
     return (
         <div>
-            <Line/>
+            <Line width={width} color={color} height={height}/>
         </div>
     )
 }
