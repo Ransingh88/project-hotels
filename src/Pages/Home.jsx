fecc4961acff50c4d1561a7b47ddcb6929b89268
Ransingh88 <rajaransingh@gmail.com>
2021-08-24 18:29:22 +0530

    home page added navbar component

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HRLine } from '../Components/HorizontalLine/HRLine'
+import { Navbar } from '../Components/Navbar/Navbar'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <HRLine/>
+        </div>
+    )
+}
+
+export  {Home}
